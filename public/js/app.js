1a0daa8657a01c2649d12af10c6c70e44c3ea2f2
raresvis <rares.visalom@gmail.com>
2015-12-12 15:03:39 +0200

    Optimum value button now works accordingly.

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,8 @@ app.controller('WikiLinksController', ['$scope', function($scope){
 	$scope.currentlyViewing = 'startPage';
 	$scope.searchInput = '';
 	$scope.currentDistance = 0;
+	$scope.optimumDistance = 5;
+	$scope.showOptimumDistance = false;
 	$scope.start = function(){
 		$scope.currentlyViewing = 'inputPage';
 	}
@@ -14,6 +16,11 @@ app.controller('WikiLinksController', ['$scope', function($scope){
 	}
 	$scope.addOption = addOption;
 	// start;
+
+	function hideButton(){
+		$scope.showOptimumDistance = true;
+	}
+	$scope.hideButton = hideButton;
 
 	window.sc = $scope;
 }]);
