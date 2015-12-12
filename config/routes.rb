0309d30fc3eaf222c47e5a2466b1681518ca1075
mihai <mihaibot_93@yahoo.com>
2015-12-12 17:26:54 +0200

    routes2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  post '/index/:name' => 'wiki#name_reurn_links_front'
-  post '/index/'=>'wiki#random_names_front'
+  post '/:name' => 'wiki#name_reurn_links_front'
+  post '/'=>'wiki#random_names_front'
 end
