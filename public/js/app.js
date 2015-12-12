472867208e8f2c091bcd169973caef0f32dc69a4
AndGrt <andreea.gurita95@yahoo.com>
2015-12-12 21:54:22 +0200

    solving confilicts

--- public/js/app.js
+++ public/js/app.js
@@ -9,19 +9,30 @@ app.controller('WikiLinksController', ['$scope', '$timeout', function($scope, $t
 	$scope.optimumDistance = 5;
 	$scope.showOptimumDistance = false;
 	$scope.links  = [];
-
+	$scope.source="";
+	$scope.destination="";
 	$scope.start = function(){
 		$scope.currentlyViewing = 'inputPage';
+		$http.get('/random')
+          .success(function(data, status, headers, config) {
+            $scope.source =  data["names"][0][0]["title"];
+            $scope.destination =  data["names"][1][0]["title"];
+
+          })
+          .error(function(data, status, headers, config) {
+            console.log('Error .');
+          });
+         
 	}
 	function addOption(){
 		$scope.currentDistance++;
+
 	}
 	$scope.addOption = addOption;
 	// start;
 
 	function hideButton(){
 		$scope.showOptimumDistance = true;
-
 		$timeout(showButton, 3000);
 	}
 	$scope.hideButton = hideButton;
