b89f4b41411e281e1d0d9d391baf385cb8853769
raresvis <rares.visalom@gmail.com>
2015-12-12 12:36:09 +0200

    Added the controller to index.html

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,12 @@
 
 var app = angular.module('WikiLinks', []);
 
-app.controller('WikiLinks', ['$scope', function($scope){
+app.controller('WikiLinksController', ['$scope', function($scope){
 	$scope.crrentlyViewing = 'startPage';
+
+	function start(){
+		$scope.currentlyViewing = 'inputPage';
+	}
+
+	$window.sc = $scope;
 }]);
