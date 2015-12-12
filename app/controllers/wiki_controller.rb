ba48778a180677db874ac1448c65c493725cc3cf
mihai <mihaibot_93@yahoo.com>
2015-12-12 13:37:32 +0200

    controller modificat

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -1,15 +1,15 @@
 class WikiController < ActionController::Base
 	def get_id
-		params_id = params[:name].to_i
-		render :json => Article.where(:title => params_id).map(&:title)
+		params_id = params[:name]
+		@id_for_name => Article.where(:title => params_id).map(&:title)
 
 	end
 	def get_id_list
 		params_id = params[:id].to_i
-		render :json => Link.where(:source_id =>params_id).map(&:destination_id)	
+		@id_destinations = Link.where(:source_id =>params_id).map(&:destination_id)	
 	end
 	def get_name
 		params_id = params[:id].to_i
-		render :json => Article.where(:id => params[:id]).map(&:title)
+		@name = Article.where(:id => params[:id])
 	end
 end
