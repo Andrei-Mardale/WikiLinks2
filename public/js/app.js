3af2b1f787c5f25d4408d5ce9b947614c92c41a3
AndGrt <andreea.gurita95@yahoo.com>
2015-12-12 13:14:16 +0200

    js

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,14 @@ var app = angular.module('WikiLinksApp', []);
 
 app.controller('WikiLinksController', ['$scope', function($scope){
 	$scope.currentlyViewing = 'startPage';
-
+	$scope.currentDistance = 0;
 	$scope.start = function(){
 		$scope.currentlyViewing = 'inputPage';
 	}
-
+	function addOption(){
+		$scope.currentDistance++;
+	}
+	$scope.addOption = addOption;
 	// start;
 
 	window.sc = $scope;
