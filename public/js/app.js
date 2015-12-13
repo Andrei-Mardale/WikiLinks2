db27fd06a33f7757c501c1b907a7ce50d13ca6d6
raresvis <rares.visalom@gmail.com>
2015-12-13 11:55:57 +0200

    Added node selection posibility.

--- public/js/app.js
+++ public/js/app.js
@@ -11,14 +11,19 @@ app.controller('WikiLinksController', ['$scope', '$timeout','$http', function($s
 	$scope.links  = [];
 	$scope.source="";
 	$scope.destination="";
+	$scope.visited=[];
+	$scope.currentLocation='';
+
 	$scope.start = function(){
 		$scope.currentlyViewing = 'inputPage';
 		$http.get('/random')
           .success(function(data, status, headers, config) {
             $scope.source =  data["names"][0][0];
             $scope.destination =  data["names"][1][0];
+            $scope.links = data["links"];
             console.log(data);
 
+            $scope.currentLocation = $scope.source;
           })
           .error(function(data, status, headers, config) {
             console.log('Error .');
@@ -41,6 +46,27 @@ app.controller('WikiLinksController', ['$scope', '$timeout','$http', function($s
 	function showButton(){
 		$scope.showOptimumDistance = false;
 	}
+
+	function choice(link){
+		$scope.currentLocation = link;
+		$scope.links = [];
+		var endpoint = '/' + link;
+
+		console.log('endpoint'+endpoint);
+
+		$http.get(endpoint)
+          .success(function(data, status, headers, config) {
+            $scope.links = data;
+          })
+          .error(function(data, status, headers, config) {
+            console.log('Error .');
+          });
+
+        $scope.currentDistance++;
+
+        $scope.visited.push(link);
+	}
+	$scope.choice = choice;
 
 	window.sc = $scope;
 }]);
